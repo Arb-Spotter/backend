d2f9c93cdf0d2e37b79bc0f4d1d061380d1f0f3e
dichouhan <dichouhan@deqode.com>
2023-09-23 22:23:23 +0530

    feat: handle errors in history endpoint

--- arb_spotter_app/views.py
+++ arb_spotter_app/views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -13,25 +14,26 @@ candle_size_model_map = {
 
 
 def tokens(request):
-    market_data = MarketData.objects.distinct('token_id')
-
+    market_data = MarketData.objects.distinct('token')
     data = list(market_data.values())
     return JsonResponse(data={"data": data}, status= 200)
     
 
 def token_details(request, token_id):
-    print(token_id)
     market_data = MarketData.objects.filter(token_id=token_id)
-
     data = list(market_data.values())
     return JsonResponse(data={"data": data}, status= 200)
 
 
 def history(request):
+    try:
 
-    candle_size = request.GET['candle_size']
-    token = request.GET['token']
-    exchange = request.GET['exchange']
+        candle_size = request.GET['candle_size']
+        token = request.GET['token']
+        exchange = request.GET['exchange']
+    except:
+        return HttpResponse("missing query parameters, required 'candle_size', 'token', 'exchange'", status=HTTPStatus.BAD_REQUEST)
+
     model = candle_size_model_map[candle_size]
     ohlcv = model.objects.filter(token=token, exchange=exchange).order_by("updatedat").values("close", "updatedat")
     return JsonResponse(data={"data": list(ohlcv)}, status= 200)
